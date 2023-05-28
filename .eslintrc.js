88a8a1219cbd466693ab4aaa1c1fa7fd548f5fa3
agger34 <hungtd.h3it@gmail.com>
2023-05-28 21:55:54 +0700

    feat: add husky hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,11 +22,11 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
-  'prettier/prettier': [
-    'error',
-    {
-      endOfLine: 'auto',
-    },
-  ],
 };
